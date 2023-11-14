56869752847dc8f467a0a3bf0d345ed4ea80bd6b
lkjsfda <149373914+lkjsfda@users.noreply.github.com>
2023-11-14 17:41:53 +0500

    Updadfte main.py
    sdfsdfsdf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-vdf
+vdfdsdfsdf
     print_hi('PyCharm')bnm
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
